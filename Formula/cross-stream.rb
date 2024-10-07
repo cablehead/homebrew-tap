a492faaad15c2dba03ff83661bc465a27270ff53
Andy Gayton <andy@thecablelounge.com>
2024-10-07 14:41:39 -0400

    fix url

--- Formula/cross-stream.rb
+++ Formula/cross-stream.rb
@@ -1,7 +1,7 @@
 class CrossStream < Formula
   desc "An event stream store for personal, local-first use, specializing in event sourcing."
   homepage "https://github.com/cablehead/xs"
-  url "https://github.com/cablehead/xs/releases/download/v0.0.8/cross-stream-macos.tar.gz"
+  url "https://github.com/cablehead/xs/releases/download/v0.0.8/cross-stream-0.0.8-macos.tar.gz"
   sha256 "4427fe154f4dfcb3ffd21cf103e9b081c15d46c0fb5ab2a4c8eff7ea5f45f1fe"
   license "MIT"
   version "0.0.8"
