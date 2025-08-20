4f910f520c3e666fcada0e9358f97f16b8e6a56d
Andy Gayton <andy@thecablelounge.com>
2025-08-20 00:51:41 +0000

    fix: correct SHA256 for macOS v0.6.0 tarball

--- Formula/cross-stream.rb
+++ Formula/cross-stream.rb
@@ -2,7 +2,7 @@ class CrossStream < Formula
   desc "An event stream store for personal, local-first use, specializing in event sourcing."
   homepage "https://github.com/cablehead/xs"
   url "https://github.com/cablehead/xs/releases/download/v0.6.0/cross-stream-v0.6.0-macos.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "842da62521680e9475edb278757b24953881bf3185f979c63b79dff8be3c60f5"
   license "MIT"
   version "0.6.0"
 
