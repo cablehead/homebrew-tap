49f4456f2190517a0bd614d712a3d433b0812f82
Andy Gayton <andy@thecablelounge.com>
2025-08-20 00:47:35 +0000

    fix: correct macOS asset filename for v0.6.0

--- Formula/cross-stream.rb
+++ Formula/cross-stream.rb
@@ -1,8 +1,8 @@
 class CrossStream < Formula
   desc "An event stream store for personal, local-first use, specializing in event sourcing."
   homepage "https://github.com/cablehead/xs"
-  url "https://github.com/cablehead/xs/releases/download/v0.6.0/cross-stream-v0.6.0-x86_64-apple-darwin.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/cablehead/xs/releases/download/v0.6.0/cross-stream-v0.6.0-macos.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "MIT"
   version "0.6.0"
 
