8c9462492e46c66a9302688ab859a0203af0ff4c
Andy Gayton <andy@thecablelounge.com>
2025-10-17 15:55:42 +0000

    chore: update cross-stream to 0.6.4

--- Formula/cross-stream.rb
+++ Formula/cross-stream.rb
@@ -1,10 +1,10 @@
 class CrossStream < Formula
   desc "An event stream store for personal, local-first use, specializing in event sourcing."
   homepage "https://github.com/cablehead/xs"
-  url "https://github.com/cablehead/xs/releases/download/v0.6.3/cross-stream-v0.6.3-macos.tar.gz"
-  sha256 "ac245cbed59aa92c982856b29e88c901fcd98baeb03ffd064b7d2be5fdab5d1b"
+  url "https://github.com/cablehead/xs/releases/download/v0.6.4/cross-stream-v0.6.4-macos.tar.gz"
+  sha256 "39ef2a21441fc4c658bfeef1c463ab88ea2903f9fda4ed3614d0dd86ca3b3241"
   license "MIT"
-  version "0.6.3"
+  version "0.6.4"
 
   def install
     bin.install "xs"
