f7a76da1ac549eb89e4d5de6fbe812f1e7600f20
Andy Gayton <andy@thecablelounge.com>
2025-12-31 22:25:07 +0000

    fix: correct sha256 for http-nu 0.7.1

--- Formula/http-nu.rb
+++ Formula/http-nu.rb
@@ -2,7 +2,7 @@ class HttpNu < Formula
   desc "The surprisingly performant, Nushell-scriptable HTTP server that fits in your back pocket"
   homepage "https://github.com/cablehead/http-nu"
   url "https://github.com/cablehead/http-nu/releases/download/v0.7.1/http-nu-darwin-arm64.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "c98e6f2d4cad6e36ac2ff623c31a5d6a573cf00ba7915f6d8ae40024600cc477"
   license "MIT"
   version "0.7.1"
 
