24b043a8b57d709d7a92926d1d33abbcadeafcc1
Andy Gayton <andy@thecablelounge.com>
2026-01-03 21:42:56 +0000

    cross-stream 0.7.1

--- Formula/cross-stream.rb
+++ Formula/cross-stream.rb
@@ -1,10 +1,10 @@
 class CrossStream < Formula
   desc "An event stream store for personal, local-first use, specializing in event sourcing."
   homepage "https://github.com/cablehead/xs"
-  url "https://github.com/cablehead/xs/releases/download/v0.7.0/cross-stream-v0.7.0-macos.tar.gz"
-  sha256 "29d652a4b010255c2f2517029e010edc427b30243ad69ec6dba3c4fc21835f30"
+  url "https://github.com/cablehead/xs/releases/download/v0.7.1/cross-stream-v0.7.1-macos.tar.gz"
+  sha256 "45e2b714ab8f09d207dbac01948603815cc43e8738489fe7a1e86694c0d0b483"
   license "MIT"
-  version "0.7.0"
+  version "0.7.1"
 
   def install
     bin.install "xs"
