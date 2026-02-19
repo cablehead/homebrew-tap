643813de7761d313f56ec08c3bc5ed9642d10313
Andy Gayton <andy@thecablelounge.com>
2026-02-19 05:10:33 +0000

    http-nu 0.12.0

--- Formula/http-nu.rb
+++ Formula/http-nu.rb
@@ -1,10 +1,10 @@
 class HttpNu < Formula
   desc "The surprisingly performant, Datastar-ready, Nushell-scriptable HTTP server that fits in your back pocket"
   homepage "https://github.com/cablehead/http-nu"
-  url "https://github.com/cablehead/http-nu/releases/download/v0.11.0/http-nu-v0.11.0-darwin-arm64.tar.gz"
-  sha256 "6a6bee4dd70a1abd7e6825d78edc711867b6d6d0a0327e59b46af5e7945c2fb6"
+  url "https://github.com/cablehead/http-nu/releases/download/v0.12.0/http-nu-v0.12.0-darwin-arm64.tar.gz"
+  sha256 "a6468a1e2bc23b0132db4f18621678aface0fd53eb500c22e1fcaeb7e1e2f2f4"
   license "MIT"
-  version "0.11.0"
+  version "0.12.0"
 
   def install
     bin.install "http-nu"
