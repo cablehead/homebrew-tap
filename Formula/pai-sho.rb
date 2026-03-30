b61a8f3caa1bde2056a7f3f6040efa154f3bdc33
Andy Gayton <andy@thecablelounge.com>
2026-03-30 17:51:27 +0000

    pai-sho: update to v0.2.0

--- Formula/pai-sho.rb
+++ Formula/pai-sho.rb
@@ -1,10 +1,10 @@
 class PaiSho < Formula
   desc "Peer-to-peer TCP port forwarding over iroh"
   homepage "https://github.com/cablehead/pai-sho"
-  url "https://github.com/cablehead/pai-sho/releases/download/v0.1.2/pai-sho-v0.1.2-macos-arm64.tar.gz"
-  sha256 "fa3b67900574344c41151b4d48a40c02c24c60d71a984b6fba6dc37051648f77"
+  url "https://github.com/cablehead/pai-sho/releases/download/v0.2.0/pai-sho-v0.2.0-macos-arm64.tar.gz"
+  sha256 "d2480e3da6b1ed7d4dc859768d0aa80021b56a61e4e2d40b9109a907f60e0ec7"
   license "MIT"
-  version "0.1.2"
+  version "0.2.0"
 
   def install
     bin.install "pai-sho"
