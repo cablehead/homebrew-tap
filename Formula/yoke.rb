e3980fe0608118bc0d91d5b2a8462eda3edbf9d7
Andy Gayton <andy@thecablelounge.com>
2026-04-05 00:08:31 +0000

    yoke 0.3.0

--- Formula/yoke.rb
+++ Formula/yoke.rb
@@ -1,10 +1,10 @@
 class Yoke < Formula
   desc "Headless agent harness. JSONL in, JSONL out."
   homepage "https://github.com/cablehead/yoke"
-  url "https://github.com/cablehead/yoke/releases/download/v0.2.2/yoke-v0.2.2-darwin-arm64.tar.gz"
-  sha256 "788871140ce3c08d332f6b60bf8508ee67e13d7e43ec5c2fa9621576be1379be"
+  url "https://github.com/cablehead/yoke/releases/download/v0.3.0/yoke-v0.3.0-darwin-arm64.tar.gz"
+  sha256 "14850c1d6bcfe844532a7bfe243e2f868612a1232446cbc5eb57f6bbaf64350b"
   license "MIT"
-  version "0.2.2"
+  version "0.3.0"
 
   def install
     bin.install "yoke"
