8c63bea8d809ec5cbb2dc133a0d836c0103fcca3
Andy Gayton <andy@thecablelounge.com>
2026-06-25 20:33:08 +0000

    stacks2099 v0.6.0

--- Formula/stacks2099.rb
+++ Formula/stacks2099.rb
@@ -1,10 +1,10 @@
 class Stacks2099 < Formula
   desc "Server-projected terminal + notes workspace in a single Nushell-powered binary"
   homepage "https://github.com/cablehead/stacks2099"
-  url "https://github.com/cablehead/stacks2099/releases/download/v0.5.0/stacks2099-aarch64-apple-darwin.tar.gz"
-  sha256 "aaa1ef8cc384e230f1fcc105242f60095219c75de848ec19d425d30a149d0915"
+  url "https://github.com/cablehead/stacks2099/releases/download/v0.6.0/stacks2099-aarch64-apple-darwin.tar.gz"
+  sha256 "75b445171d4896275719134069e933c4c11d404b2efbe8b48a28003a391b315e"
   license "MIT"
-  version "0.5.0"
+  version "0.6.0"
 
   def install
     bin.install "stacks2099"
