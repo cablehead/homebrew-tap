76d8ccb6621a7cebec7451d47902446c2f1ccec8
Andy Gayton <andy@thecablelounge.com>
2026-07-31 00:39:01 +0000

    ptyzzz: declare MIT license

--- Formula/ptyzzz.rb
+++ Formula/ptyzzz.rb
@@ -3,6 +3,7 @@ class Ptyzzz < Formula
   homepage "https://github.com/cablehead/ptyZZZ"
   url "https://github.com/cablehead/ptyZZZ/releases/download/v0.0.1/ptyZZZ-v0.0.1-macos-arm64.tar.gz"
   sha256 "4fb6fcb24fcb64c3890a05bf2fcedf5219f1f70de7abf9fb739fdd60f80c1f8f"
+  license "MIT"
   version "0.0.1"
 
   def install
