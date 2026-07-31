66c9e4eb417b35f84e7b4b143db230ce27dced1e
Andy Gayton <andy@thecablelounge.com>
2026-07-31 00:36:04 +0000

    ptyzzz 0.0.1

---
+++ Formula/ptyzzz.rb
@@ -0,0 +1,15 @@
+class Ptyzzz < Formula
+  desc "A terminal as a unix pipe: pty in, JSONL screen frames out"
+  homepage "https://github.com/cablehead/ptyZZZ"
+  url "https://github.com/cablehead/ptyZZZ/releases/download/v0.0.1/ptyZZZ-v0.0.1-macos-arm64.tar.gz"
+  sha256 "4fb6fcb24fcb64c3890a05bf2fcedf5219f1f70de7abf9fb739fdd60f80c1f8f"
+  version "0.0.1"
+
+  def install
+    bin.install "ptyZZZ"
+  end
+
+  test do
+    system "#{bin}/ptyZZZ", "--help"
+  end
+end
