afd8c2e4396e0548268037fb853e283bb08b0348
Andy Gayton <andy@thecablelounge.com>
2026-08-07 13:35:31 +0000

    pai-sho: drop redundant version line (scanned from URL)

--- Formula/pai-sho.rb
+++ Formula/pai-sho.rb
@@ -4,7 +4,6 @@ class PaiSho < Formula
   url "https://github.com/cablehead/pai-sho/releases/download/v0.4.0/pai-sho-v0.4.0-macos-arm64.tar.gz"
   sha256 "4290f9038925ada451dc47f3c90fb60a13d76e0982fec1da33e6693b660361a5"
   license "MIT"
-  version "0.4.0"
 
   def install
     bin.install "pai-sho"
